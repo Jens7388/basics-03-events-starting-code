54363cad88eeaf2614c61d884422cf432c0a4cd3
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-11-25 13:28:55 +0100

    added event handlers

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
     setName(event){
       this.name = event.target.value;  
+    },
+    submitForm(event){
+      
+    },
+    confirmName(){
+      this.confirmedName = this.name;
     },
     add(num){
         this.counter += num;
